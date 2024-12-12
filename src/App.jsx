3ee7a5523c2430bf4ce25c3a9bfa84414258f470
Lorrapaz <lorrannyyasmin62gmail.com>
2024-12-12 10:37:54 -0300

    PORRA2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Header } from './Post';
 import { Searcher } from './Search';
 import './styles.css';
--- src/Search.jsx
+++ src/Search.jsx
@@ -16,41 +16,38 @@ export function Searcher() {
   );
 
   return (
-    <main>
-      <div className="container">
-        <div className="logo-container">
-          <img src={Icon} alt="Luluzinha Logo" className="logo" />
-          <h1 className="title">LULUZINHA</h1>
-        </div>
-        <div className="search-container">
-          <div className="Pesquisar">
-            <div className="search-icon"></div>
-            <input
-              id="search-input"
-              type="text"
-              value={busca}
-              placeholder="Pesquisar"
-              onChange={(ev) => setBusca(ev.target.value)}
-              onFocus={() => setFocus(true)}
-              onBlur={() => setTimeout(() => setFocus(false), 200)}
-              className="search-input"
-            />
-          </div>
-        </div>
-        {focus && filterFiltrados.length > 0 && (
-          <div className="results-container">
-            {filterFiltrados.map((item, index) => (
-              <div key={index} className="result-item">
-                {item}
-              </div>
-            ))}
-          </div>
-        )}
-
-        <div className="notification-container">
-          <i className="notification-icon"></i>
-        </div>
+    <div className="container">
+      <div className='Super-container'><div className="logo-container">
+        <img src={Icon} alt="Luluzinha Logo" className="logo" />
+        <h1 className="title">LULUZINHA</h1></div>
       </div>
-    </main>
+      <div className="search-container">
+        <div className='Pesquisar'>
+          <div className="search-icon"></div>
+          <input
+          id="search-input"
+          type="text"
+          value={busca}
+          placeholder="Pesquisar"
+          onChange={(ev) => setBusca(ev.target.value)}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setTimeout(() => setFocus(false), 200)}
+          className="search-input"
+          />
+        </div>
+        <div>{focus && filterFiltrados.length > 0 && (
+        <div className="results-container">
+          {filterFiltrados.map((item, index) => (
+            <div key={index} className="result-item">
+              {item}
+            </div>
+          ))}
+        </div>
+        )}</div>
+      </div>
+      <div className="notification-container">
+        <i className="notification-icon"></i>
+      </div>
+    </div>
   );
 }
