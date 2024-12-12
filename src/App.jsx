9fd9968d117e66a786728be6db8504f42ebc59ce
Emilly <emilly2008lemos@gmail.com>
2024-12-12 10:23:46 -0300

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from './Post';
 import { Searcher } from './Search';
 import './styles.css';
 
-export default function App() {
+export function App() {
   return (
     <>
       <Header />
